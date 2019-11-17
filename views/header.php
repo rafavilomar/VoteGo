b703999574cca6404474f8dbf98cfe8877639865
Rafael <rafaelvm2707@gmail.com>
2019-11-17 15:04:25 -0400

    header y footer

---
+++ views/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <title>VoteGo</title>
+</head>
+<body>
+<div class="container">
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#"><span><img class="icono" src="../Iconos/home_blanco.svg" alt=""></span>Inicio </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Mesa Electoral</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Casilla de Votación</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Configuración</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
+    </header>
+</div>
