0cf0fb042322bc71cd7b7b816ca149a8cb384ba1
3DWIN13 <ecolladocastillo@gmail.com>
2019-12-02 20:16:59 -0400

    Update Mesa.php

--- application/controllers/Mesa.php
+++ application/controllers/Mesa.php
@@ -6,7 +6,20 @@ class Mesa extends CI_Controller {
     public function sesion()
     {
 		$this->load->view('mesaElectoral/login');
-    }
+	}
+	
+
+	/************************la casilla(nuevo)************************************************ */
+	public function casilla()
+    {
+		$includes['header'] = $this->load->view('includes/header','',TRUE);
+		$includes['footer'] = $this->load->view('includes/footer','',TRUE);
+		
+		$this->load->view('CasillaVotacion/verificarVotantes',$includes);
+	}
+	/****************************************************************************************** */
+	
+
 	public function validar()
 	{
 		$includes['header'] = $this->load->view('includes/header','',TRUE);
