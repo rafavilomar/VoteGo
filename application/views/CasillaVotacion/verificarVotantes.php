6f17e203304ee6b129f77a1991373ead670f0ac8
3DWIN13 <ecolladocastillo@gmail.com>
2019-12-02 20:17:07 -0400

    Create verificarVotantes.php

---
+++ application/views/CasillaVotacion/verificarVotantes.php
@@ -0,0 +1,36 @@
+
+
+<?= $header ?>
+
+<br><br><br>
+<div class="contenedor">
+ <div class="text-center">
+    <button style="margin-top:200px" class="btn btn-outline-info btn-lg btn1" type="button" data-toggle="modal" data-target="#modalV">Verificar Cedula</button>
+</div>
+<!-- Modal -->
+<form action="" method="GET" id="frm2">
+    <div class="modal fade" id="modalV" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="ModalTitle">Verificacion de cédula</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div>
+                        <input class="form-control text-center" type="text" placeholder="Ingrese la Cedula" name="cedula" id="cedula" onKeyPress="return soloNumeros(event)">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                    <button type="submit" class="btn btn-primary" name="V" id="V">Verificar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+</div>
+<!--Fin de Modal----------->
+<?= $footer ?>
