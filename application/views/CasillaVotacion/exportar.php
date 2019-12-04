09f49e7187608b0c73caf97834d1c7d73a819c1f
3DWIN13 <ecolladocastillo@gmail.com>
2019-12-04 06:15:30 -0400

    Update exportar.php

--- application/views/CasillaVotacion/exportar.php
+++ application/views/CasillaVotacion/exportar.php
@@ -11,17 +11,18 @@ $pdf->SetFont('Arial', 'B', 15);
 $pdf->Cell(20,10, '',0,0,'C');
 $pdf->Cell(50,6,'PARTIDOS', 1, 0, 'C',1);
 $pdf->Cell(50,6,'NOMBRES', 1, 0, 'C',1);
-$pdf->Cell(50,6,'CARGOS', 1, 0, 'C',1);
+$pdf->Cell(50,6,'CARGOS', 1, 1, 'C',1);
 
-/*
+
 $pdf->SetFont('Arial', '', 10);
 
-$us = cargar();
-foreach ($us as $mostrar){
-$pdf->Cell(38,6,$mostrar['cedula'], 1, 0, 'C');
-$pdf->Cell(38,6,$mostrar['nombre'], 1, 0, 'C');
-$pdf->Cell(38,6,$mostrar['apellido'], 1, 0, 'C');
-$pdf->Cell(38,6,$mostrar['fechaN'], 1, 0, 'C');
-*/
+
+foreach ($mivoto as $mostrar){
+
+$pdf->Cell(20,10,'', 0, 0, 'C');
+$pdf->Cell(50,6,$mostrar->nombreC, 1, 0, 'C');
+$pdf->Cell(50,6,$mostrar->partidoC, 1, 0, 'C');
+$pdf->Cell(50,6,$mostrar->cargoC, 1, 1, 'C');
+}
 
 $pdf->Output();
