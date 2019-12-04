549cca587515e0058db74210a056be0b8dc34e32
3DWIN13 <ecolladocastillo@gmail.com>
2019-12-03 23:58:31 -0400

    Create modelo.php

---
+++ application/models/modelo.php
@@ -0,0 +1,35 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class modelo extends CI_Controller {
+    public function __construct()
+	{
+		parent::__construct();	
+        $this->load->database();
+		
+    }
+
+    function cargarCandidatos(){
+        $this->db->select('*');
+        $this->db->from('candidatos');
+        $this->db->join('partidos','candidatos.id_partidos=partidos.id_partidos');
+        $this->db->join('niveles','candidatos.id_niveles=niveles.id_niveles');
+        return  $this->db->get();
+
+        
+        /*foreach ($query->result() as $key) {
+            echo $key->nombre.'<br>';
+            echo $key->nombres.'<br>';
+
+        }
+
+       /* $query = $this->db->get();
+
+        return $query->result();*/
+    }
+
+}
+
+/* End of file Controllername.php */
